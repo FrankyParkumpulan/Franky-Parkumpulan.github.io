f43a2024f73a026c4c65bd5dd1a70c0b3ab30058
Franky Parkumpulan <36655906+FrankyParkumpulan@users.noreply.github.com>
2018-03-06 13:20:14 +0700

    Update and rename EXE3.html to exe3.js

---
+++ Week2/exe3.js
@@ -0,0 +1,24 @@
+var nama = "";
+var peran = "";
+
+if (nama === ``) {
+console.log(`nama harus diisi!`);
+  }
+  else if (peran === "Fighter") {
+    console.log("Selamat datang di Dunia Proxytia");
+    console.log("Halo", nama + ` ` + peran);
+    console.log("kamu dapat menyerang dengan senjatamu!");
+    
+  }
+  else if (peran === "Priestess") {
+    console.log("Selamat datang di Dunia Proxytia");
+    console.log("Halo", nama + ` ` + peran);
+    console.log("kamu akan membantu temanmu yang terluka.");
+} 
+
+else if (peran === "Miracle") {
+    console.log("Selamat datang di Dunia Proxytia");
+    console.log("Halo", nama + ` ` + peran);
+    console.log("ciptakan keajaiban yang membantu kemenanganmu!");
+
+}
